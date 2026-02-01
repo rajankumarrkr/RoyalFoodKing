3fcc4ea3b6f89ae6d3d99a92c0ee90fa88a265ae
rajankumarrkr <rajankumarrkr4576@gmail.com>
2026-02-01 13:39:10 +0530

    pwa implement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 console.log("Royal Food King: Mounting Application...");
 
